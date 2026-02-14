30d0f99bec75cfd596b238202148e9907f221317
sanskriti060906-git <sanskriti060906@gmail.com>
2026-02-14 13:18:11 +0530

    Add matrix symmetry check program
    Implement a program to check if a matrix is symmetric.

---
+++ Day12.c
@@ -0,0 +1,27 @@
+// Write a program to check whether a given matrix is symmetric. A matrix is said to be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
+
+#include <stdio.h>
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int a[m][n];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+    for(int i = 0; i < m; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+    printf("Symmetric Matrix");
+    return 0;
+}
