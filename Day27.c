a02e39a89064365fed12396a8856d85f666a4d28
sanskriti060906-git <sanskriti060906@gmail.com>
2026-02-27 19:22:38 +0530

    Add linked list intersection point finder
    Implement linked list intersection finder with dynamic memory allocation.

---
+++ Day27.c
@@ -0,0 +1,70 @@
+//Find Intersection Point of Two Linked Lists - Implement using linked list with dynamic memory allocation.
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next)
+        temp = temp->next;
+    temp->next = newNode;
+}
+int getLength(struct Node* head) {
+    int count = 0;
+    while (head) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+int findIntersection(struct Node* head1, struct Node* head2) {
+    int len1 = getLength(head1);
+    int len2 = getLength(head2);
+    int diff = abs(len1 - len2);
+    if (len1 > len2)
+        while (diff--) head1 = head1->next;
+    else
+        while (diff--) head2 = head2->next;
+    while (head1 && head2) {
+        if (head1->data == head2->data)
+            return head1->data;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return -1;
+}
+int main() {
+    int n, m, x;
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        insertEnd(&head1, x);
+    }
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &x);
+        insertEnd(&head2, x);
+    }
+    int result = findIntersection(head1, head2);
+    if (result == -1)
+        printf("No Intersection");
+    else
+        printf("%d", result);
+    return 0;
+}
