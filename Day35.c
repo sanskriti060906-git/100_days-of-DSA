2659985875ac5a7db3ef56d6ae0d3923faa17bd8
sanskriti060906-git <sanskriti060906@gmail.com>
2026-03-07 10:16:37 +0530

    Implement queue using array

---
+++ Day35.c
@@ -0,0 +1,38 @@
+//Queue Using Array - Implement using linked list with dynamic memory allocation.
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+void enqueue(int value)
+{
+    if (rear == MAX - 1)
+    {
+        return;
+    }
+    if (front == -1)
+        front = 0;
+    rear++;
+    queue[rear] = value;
+}
+void display()
+{
+    int i;
+    for (i = front; i <= rear; i++)
+    {
+        printf("%d ", queue[i]);
+    }
+}
+int main()
+{
+    int n, x, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+    display();
+    return 0;
+}
