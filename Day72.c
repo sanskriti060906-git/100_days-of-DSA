75db80b1ac94aaaad8da11d4a7c6bbd7f8bcc2ae
sanskriti060906-git <sanskriti060906@gmail.com>
2026-04-13 08:03:38 +0530

    Add function to find first repeated character
    Implement a program to find the first repeated character in a string.

---
+++ Day72.c
@@ -0,0 +1,19 @@
+//Given a string s consisting of lowercase letters, find the first repeated character in the string. A character is considered repeated if it appears more than once, and among all such characters, the one whose second occurrence has the smallest index should be returned.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+    int seen[26] = {0};  // for lowercase letters
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+        if (seen[index] == 1) {
+            printf("%c", s[i]);
+            return 0;
+        }
+        seen[index] = 1;
+    }
+    printf("-1");
+    return 0;
+}
