9f61c59cc0add6e78e7d5749aeae5b5229986a6e
sanskriti060906-git <sanskriti060906@gmail.com>
2026-05-02 09:27:57 +0530

    Sort an array using insertion sort. Stable sort. Good for nearly sorted arrays.
    Implement insertion sort algorithm and array printing function.

---
+++ Day93.c
@@ -0,0 +1,27 @@
+//Problem: Sort an array using insertion sort.
+Stable sort. Good for nearly sorted arrays.
+
+#include <stdio.h>
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i]; 
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+int main() {
+    int arr[] = {5, 2, 4, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
