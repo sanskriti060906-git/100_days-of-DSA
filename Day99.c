aa6b1f97a235f953340aa99caf10e55a5beb5190
sanskriti060906-git <sanskriti060906@gmail.com>
2026-05-04 07:35:41 +0530

    Add car fleet calculation
    Implement car fleet calculation based on positions and speeds.

---
+++ Day99.c
@@ -0,0 +1,43 @@
+//Problem: Given a target distance and cars’ positions & speeds, compute the number of car fleets reaching the destination.
+Sort cars by position in descending order and calculate time to reach target.
+  #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int position;
+    int speed;
+} Car;
+int compare(const void *a, const void *b) {
+    Car *c1 = (Car *)a;
+    Car *c2 = (Car *)b;
+    return c2->position - c1->position;
+}
+int carFleet(int target, int position[], int speed[], int n) {
+    Car cars[n];
+    for (int i = 0; i < n; i++) {
+        cars[i].position = position[i];
+        cars[i].speed = speed[i];
+    }
+    qsort(cars, n, sizeof(Car), compare);
+    int fleets = 0;
+    double maxTime = 0.0;
+    for (int i = 0; i < n; i++) {
+        double time = (double)(target - cars[i].position) / cars[i].speed;
+        if (time > maxTime) {
+            fleets++;
+            maxTime = time;
+        }
+    }
+    return fleets;
+}
+int main() {
+    int target = 12;
+    int position[] = {10, 8, 0, 5, 3};
+    int speed[] = {2, 4, 1, 1, 3};
+    int n = sizeof(position) / sizeof(position[0]);
+
+    int result = carFleet(target, position, speed, n);
+    printf("%d\n", result);
+
+    return 0;
+}
